f711d64de5edc0ae933ffef936e0946d491589cf
Stavichh <98745850+Stavichh@users.noreply.github.com>
2023-12-20 20:33:37 +0700

    2023/09/21 00:00|Практическое задание по уроку "Строки и индексация строк"

---
+++ main.py
@@ -0,0 +1,14 @@
+
+my_string = "goddamn"
+another_string = "i'm full"
+
+def main():
+ print(my_string[0])
+ print(my_string[-1])
+ print(my_string[3:5])
+ print(len(my_string))
+ new_string = my_string + " " + another_string
+ print(new_string)
+
+
+main()
